3cc29dc92c090867ce13c75aba94cf84259e0d39
allankouidri <allan.kouidri@gmail.com>
2024-08-26 12:02:28 +0200

    Save quantized FLUX model

--- utils/load_model.py
+++ utils/load_model.py
@@ -1,8 +1,15 @@
 import torch
-from huggingface_hub import login
 from diffusers import FluxTransformer2DModel, FluxPipeline
 from transformers import T5EncoderModel
-from optimum.quanto import freeze, qfloat8, quantize
+from optimum.quanto import freeze, qfloat8, quantize, QuantizedDiffusersModel, QuantizedTransformersModel
+import os
+
+
+class QuantizedFlux2DModel(QuantizedDiffusersModel):
+    base_class = FluxTransformer2DModel
+
+class QuantizedT5Model(QuantizedTransformersModel):
+    auto_class = T5EncoderModel
 
 
 def check_float16_and_bfloat16_support():
@@ -24,31 +31,45 @@ def get_model_info(parameters):
         model_lk = "https://huggingface.co/Kijai/flux-fp8/blob/main/flux1-dev-fp8.safetensors"
         token = None
         repo = "black-forest-labs/FLUX.1-dev"
+        model_version = "dev"
 
     if parameters.model_name=='flux1-schnell-fp8':
         model_lk = "https://huggingface.co/Kijai/flux-fp8/blob/main/flux1-schnell-fp8.safetensors"
         token = parameters.token
         repo = "black-forest-labs/FLUX.1-schnell"
+        model_version = "schnell"
 
-    return model_lk, repo, token
+    return model_lk, repo, token, model_version
 
 
 def load_pipe(param, folder_path):
-    model_link, bfl_repo, token_hf = get_model_info(param)
+    model_link, bfl_repo, token_hf, model_type = get_model_info(param)
 
     float16_support, bfloat16_support = check_float16_and_bfloat16_support()
     dtype = torch.bfloat16 if bfloat16_support else torch.float16 \
                         if float16_support else torch.float32
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    print('Preparing models for FP8 inference, this may take a while...')
-    transformer = FluxTransformer2DModel.from_single_file(
-                                            model_link,
-                                            torch_dtype=dtype,
-                                            cache_dir=folder_path)
-    quantize(transformer, weights=qfloat8)
-    freeze(transformer)
+    # Check if the folder exists and that there is a safetensors model in the folder
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
+    safetensor_model_path = os.path.join(folder_path, f"flux_{model_type}_qfp8", "diffusion_pytorch_model-00001-of-00002.safetensors")
+    if not os.path.exists(safetensor_model_path):
+        print(f"Preparing the FLUX {model_type} model for FP8 inference, this may take a while...")
+        transformer = FluxTransformer2DModel.from_single_file(
+            model_link,
+            torch_dtype=dtype,
+            cache_dir=folder_path
+        )
+        qtransformer = QuantizedFlux2DModel.quantize(transformer, weights=qfloat8)
+
+        print(f"Saving quantized FLUX {model_type} model...")
+        qtransformer.save_pretrained(f"{folder_path}/flux_{model_type}_qfp8")
+    else:
+        print(f'Quantized FLUX {model_type} model available at {folder_path}/flux_{model_type}_qfp8')
+
+    print('Preparing the T5 encoder  model for FP8 inference, this may take a while...')
     text_encoder_2 = T5EncoderModel.from_pretrained(
                                         bfl_repo,
                                         subfolder="text_encoder_2",
@@ -58,15 +79,20 @@ def load_pipe(param, folder_path):
     quantize(text_encoder_2, weights=qfloat8)
     freeze(text_encoder_2)
 
+    # Loading
+    print("Loading FLUX pipeline...")
+    qtransformer = QuantizedFlux2DModel.from_pretrained(f"{folder_path}/flux_{model_type}_qfp8")
+    qtransformer.to(device=device, dtype=dtype)
+
     pipe = FluxPipeline.from_pretrained(
                                 bfl_repo,
                                 transformer=None,
                                 text_encoder_2=None,
                                 torch_dtype=dtype)
-    pipe.transformer = transformer
+    pipe.transformer = qtransformer
     pipe.text_encoder_2 = text_encoder_2
-
     pipe.to(device)
+
     if param.enable_model_cpu_offload:
         pipe.enable_model_cpu_offload()
 
